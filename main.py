c3006db62396dd198f27967ff2e47ade477446de
tremblayraphael29 <45863736+tremblayraphael29@users.noreply.github.com>
2021-01-25 17:20:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -50,5 +50,5 @@ connectToWifiAndUpdate()
 startApp()
 
 tim0 = Timer(0)
-tim0.init(period=1, mode=Timer.PERIODIC, callback=lambda t: index_ms())
+tim0.init(period=3, mode=Timer.PERIODIC, callback=lambda t: index_ms())
 
